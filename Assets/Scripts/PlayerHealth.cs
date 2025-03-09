02a55bba4e612230aeee02e97592909bee9da7d6
Mrmongoose64 <xminecraftclubx@gmail.com>
2025-03-09 20:13:16 +0000

    Health script + testing in level 1

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int totalHealth = 3;
+    public int currentHealth;
+    public Image[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = totalHealth;
+    }
+
+    public void TakeDamage(int damageTaken)
+    {
+        currentHealth -= damageTaken;
+
+        if (currentHealth <= 0)
+        {
+            // Death process here
+            
+        }
+        UpdateHearts(currentHealth);
+    }
+
+    public void Heal(int healthObtained)
+    {
+        if (currentHealth < totalHealth)
+        {
+            currentHealth += healthObtained;
+        }
+        UpdateHearts(currentHealth);
+    }
+
+    public void UpdateHearts(int currentHealth)
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].enabled = (i < currentHealth);
+        }
+    }
+}
