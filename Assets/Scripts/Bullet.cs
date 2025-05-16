2ad376ac832205d977b424d951d4695cf37f6f03
JPLDN <jujumaster2603@gmail.com>
2025-05-16 23:50:50 +0100

    Finished tri-shot pick-up

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 20f;
-    public Rigidbody bulletbody;
     // Start is called before the first frame update
     void Start()
     {
-        bulletbody.velocity = transform.right * speed;
+
     }
 
     // Update is called once per frame
