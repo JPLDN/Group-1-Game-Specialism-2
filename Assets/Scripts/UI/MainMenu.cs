39440e13254bb5a8d033a7ed59999acde65dd68e
Mrmongoose64 <xminecraftclubx@gmail.com>
2025-05-20 23:24:11 +0100

    Added explanation to arcade mode

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,10 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject arcadePanel;
+
     // Loads first cutscene
     public void StartButton()
     {
         SceneManager.LoadScene(2);
+    }
+
+    // Displays panel explaining the lack of arcade mode
+    public void ArcadeButton()
+    {
+        arcadePanel.SetActive(true);
+        Invoke("HideArcadePanel", 4.0f);
+    }
+
+    // Hides the panel after being invoked
+    public void HideArcadePanel()
+    {
+        arcadePanel.SetActive(false);
     }
 
     // Loads settings menu scene
